87179bbf7d11606bb92de64e33671f30acdca810
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-01-30 19:50:45 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// using JS get attribute
+// var myattri = document.querySelector("a").getAttribute("href");
+// console.log(myattri);
+
+// using jQuery get attribute
+// $("a").attr("href");
+
+// using JS remove attribute
+// document.querySelector("a").removeAttribute("href");
+
+// using jQuery remove attribute
+// $("a").removeAttr("href");
+
+// using JS set attribute
+// document.querySelector("a").setAttribute("href", "https://www.facebook.com");
+
+// using jQuery set attribute
+$("a").attr("href", "https://www.facebook.com");
